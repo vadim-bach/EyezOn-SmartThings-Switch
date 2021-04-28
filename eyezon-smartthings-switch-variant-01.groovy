6a387afce56d6de321b2f5459ba08bae4838f821
Vadim Bach <33889138+vadim-bach@users.noreply.github.com>
2021-04-28 18:16:43 -0400

    Add support for "Night Arm" mode

--- eyezon-smartthings-switch-variant-01.groovy
+++ eyezon-smartthings-switch-variant-01.groovy
@@ -14,7 +14,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at:
  *
- *		http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
@@ -26,43 +26,52 @@ APP CONSTANTS (how ST doesn't support one of the most basic constructs of
 modern programming is beyond me)
 */
 def EYEZON_URI() {
-	return "https://www.eyez-on.com"
+    return "https://www.eyez-on.com"
 }
 def EYEZON_PATH() {
-	return "/EZMOBILE/index.php"
+    return "/EZMOBILE/index.php"
 }
 def STATUS_READY() {
-	return "Ready"
+    return "Ready"
 }
 def STATUS_BUSY() {
-	return "Busy"
+    return "Busy"
 }
 def STATUS_EXIT_DELAY() {
-	return "Exit Delay"
+    return "Exit Delay"
 }
 def STATUS_AWAY_ARMED() {
-	return "Away Armed"
+    return "Away Armed"
 }
 def STATUS_STAY_ARMED() {
-	return "Stay Armed"
+    return "Stay Armed"
+}
+def STATUS_NIGHT_ARMED() {
+    return "Night Armed"
 }
 def STATUS_UNKNOWN() {
-	return "Unknown"
+    return "Unknown"
 }
 def OPERATION_ARM_STAY() {
-	return "armstay"
+    return "armstay"
+}
+def OPERATION_ARM_NIGHT() {
+    return "armnight"
 }
 def OPERATION_ARM_AWAY() {
-	return "armaway"
+    return "armaway"
 }
 def OPERATION_DISARM() {
-	return "disarm"
+    return "disarm"
 }
 def ARM_MODE_STAY() {
-	return "Stay"
+    return "Stay"
+}
+def ARM_MODE_NIGHT() {
+    return "Night"
 }
 def ARM_MODE_AWAY() {
-	return "Away"
+    return "Away"
 }
 
 preferences {
@@ -72,7 +81,7 @@ preferences {
         input "part", "number", title: "Partition #", defaultValue: 1, required: true
         input "partName", "text", title: "Partition Label (as it appears in EyezOn)", defaultValue: "Partition 1", required: true
         input "pin", "password", title: "Disarm PIN", required: true
-        input "mode", "enum", title: "Arm Mode", options: [ARM_MODE_STAY(), ARM_MODE_AWAY()]
+        input "mode", "enum", title: "Arm Mode", options: [ARM_MODE_STAY(), ARM_MODE_NIGHT(), ARM_MODE_AWAY()]
         input "exitDelay", "number", title: "System exit delay (in seconds, default is 60)", defaultValue: 60, range: "1..180", required: true
     }
 }
@@ -90,40 +99,40 @@ metadata {
     }
 
     // UI tile definitions
-	tiles(scale: 2) {
-		multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4){
-			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
+    tiles(scale: 2) {
+        multiAttributeTile(name:"switch", type: "generic", width: 6, height: 4){
+            tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
                                 attributeState "on", label:'Armed', action:"switch.off", icon:"st.security.alarm.alarm", backgroundColor:"#00A0DC", nextState: "busy"
-				attributeState "off", label:'Disarmed', action:"switch.on", icon:"st.security.alarm.clear", backgroundColor:"#ffffff", nextState: "exiting"
-				attributeState "busy", label:'Busy', icon:"st.security.alarm.alarm", backgroundColor:"#ffa81e"
-				attributeState "exiting", label:'Exiting', action:"switch.off", icon:"st.security.alarm.alarm", backgroundColor:"#ffa81e", nextState: "busy"
-			}
+                attributeState "off", label:'Disarmed', action:"switch.on", icon:"st.security.alarm.clear", backgroundColor:"#ffffff", nextState: "exiting"
+                attributeState "busy", label:'Busy', icon:"st.security.alarm.alarm", backgroundColor:"#ffa81e"
+                attributeState "exiting", label:'Exiting', action:"switch.off", icon:"st.security.alarm.alarm", backgroundColor:"#ffa81e", nextState: "busy"
+            }
             tileAttribute ("device.alarmActivity", key: "SECONDARY_CONTROL") {
                 attributeState("default", label:'Updated ${currentValue}')
             }
-		}
-		standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-			state "default", label:"", action:"getSystemStatus", icon:"st.secondary.refresh"
-		}
-		main "switch"
-		details(["switch", "refresh"])
-	}
+        }
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", label:"", action:"getSystemStatus", icon:"st.secondary.refresh"
+        }
+        main "switch"
+        details(["switch", "refresh"])
+    }
 }
 
 def initialize() {
-	sendEvent(name: "DeviceWatch-Enroll", value: "{\"protocol\": \"LAN\", \"scheme\":\"untracked\", \"hubHardwareId\": \"${device.hub.hardwareID}\"}", displayed: false)
+    sendEvent(name: "DeviceWatch-Enroll", value: "{\"protocol\": \"LAN\", \"scheme\":\"untracked\", \"hubHardwareId\": \"${device.hub.hardwareID}\"}", displayed: false)
     sendEvent(name: "DeviceWatch-DeviceStatus", value: "online")
     sendEvent(name: "healthStatus", value: "online")
 }
 
 void installed() {
-	log.debug "installed()"
-	initialize()
+    log.debug "installed()"
+    initialize()
 }
 
 def updated() {
-	log.debug "updated()"
-	initialize()
+    log.debug "updated()"
+    initialize()
 }
 
 def getSystemStatus() {
@@ -140,34 +149,37 @@ def getSystemStatus() {
         ] 
         httpGet(params) { resp ->
             textData = resp.data.getText()
-    	}
+        }
     } catch (e) {
-    	log.error 'Unable to get current system status', e
-    	throw e
+        log.error 'Unable to get current system status', e
+        throw e
     }
 
     // SmartThings doesn't support regex: https://community.smartthings.com/t/unable-to-use-matcher-methods/1280
     // So we have to do this the dumb/brittle way
     def systemStatus = STATUS_UNKNOWN()
     if (textData.contains("${settings.partName} : <span style=\"color:green;\"> Ready")) {
-    	systemStatus = STATUS_READY()
+        systemStatus = STATUS_READY()
         sendEvent(name: "switch", value: "off")
     } else if (textData.contains("${settings.partName} : <span style=\"color:black; \"> Busy")) {
-    	systemStatus = STATUS_BUSY()
+        systemStatus = STATUS_BUSY()
         sendEvent(name: "switch", value: "busy")
     } else if (textData.contains("${settings.partName} : <span style=\"color:orange;\"> Exit Delay")) {
-    	systemStatus = STATUS_EXIT_DELAY()
+        systemStatus = STATUS_EXIT_DELAY()
         sendEvent(name: "switch", value: "exiting")
     } else if (textData.contains("${settings.partName} : <span style=\"color:red;\"> Away Armed")) {
-    	systemStatus = STATUS_AWAY_ARMED()
+        systemStatus = STATUS_AWAY_ARMED()
         sendEvent(name: "switch", value: "on")
     } else if (textData.contains("${settings.partName} : <span style=\"color:red;\"> Stay Armed")) {
-    	systemStatus = STATUS_STAY_ARMED()
+        systemStatus = STATUS_STAY_ARMED()
+        sendEvent(name: "switch", value: "on")
+    } else if (textData.contains("${settings.partName} : <span style=\"color:red;\"> Night Armed")) {
+        systemStatus = STATUS_NIGHT_ARMED()
         sendEvent(name: "switch", value: "on")
     } else {
-    	log.error "Unable to determine system status from response: ${textData}. Setting to Unknown."
+        log.error "Unable to determine system status from response: ${textData}. Setting to Unknown."
     }
-	
+    
     log.info "Determined system status to be ${systemStatus} at ${timeString}"
     sendEvent(name: "alarmActivity", value: timeString, descriptionText: text, displayed: true)
     return systemStatus
@@ -177,23 +189,27 @@ def performOperation(operation) {
     log.info "Received request to perform operation: ${operation}"
     def timeString = new Date().format("MM-dd-yy h:mm a", location.timeZone)
     try {
-     	def random = new Random().nextInt(99999999) + 1
+        def random = new Random().nextInt(99999999) + 1
         def waitTime = exitDelay
         def path = "${EYEZON_URI()}${EYEZON_PATH()}?mid=${settings.mid}&action=s&did=${settings.did}&type=15&dmdben=f&rand=${random}"
         def body = "part=${settings.part}&pin=${settings.pin}"
         if (operation == OPERATION_DISARM()) {
-        	body += "&extaction=hdisarm"
+            body += "&extaction=hdisarm"
             sendEvent(name: "switch", value: "busy")
             waitTime = 5  // time in seconds
         } else if (operation == OPERATION_ARM_STAY()) {
-        	body += "&extaction=dohpincommand&hextaction=harmstay"
+            body += "&extaction=dohpincommand&hextaction=harmstay"
+            sendEvent(name: "switch", value: "exiting")
+        } else if (operation == OPERATION_ARM_NIGHT()) {
+            body += "&extaction=dohpincommand&hextaction=harmnight"
+            sendEvent(name: "switch", value: "exiting")
+        } else if (operation == OPERATION_ARM_AWAY()) {
+            body += "&extaction=dohpincommand&hextaction=harmaway"
             sendEvent(name: "switch", value: "exiting")
         } else {
-        	body += "&extaction=dohpincommand&hextaction=harmaway"
-            sendEvent(name: "switch", value: "exiting")
+            log.error "Unable to perform operation ${operation}: operation is unknown."
+            return
         }
-        //log.info "Path: ${path}"
-        //log.info "Body: ${body}"
         httpPost(path, body)
         log.info "Operation ${operation} successfully submitted, now waiting for system exit/disarm delay (${waitTime} sec) for confirmation..."
         runIn(waitTime, getSystemStatus, [overwrite: true])  // wait for system delay and then get system status to update the tile, and overwrite any pending schedules
@@ -207,41 +223,55 @@ def on() {
     log.info "Received request to arm system. Mode: ${settings.mode}"
     def systemStatus = getSystemStatus()
     if (systemStatus != STATUS_READY()) {
-    	log.error "Cannot arm system. Expecting status ${STATUS_READY()}. Actual status: ${systemStatus}"
+        log.error "Cannot arm system. Expecting status ${STATUS_READY()}. Actual status: ${systemStatus}"
         return
     }
-    def operation = settings.mode == 'Away' ? OPERATION_ARM_AWAY() : OPERATION_ARM_STAY()
+    def operation
+    if (settings.mode == ARM_MODE_AWAY()) {
+        operation = OPERATION_ARM_AWAY()
+    } else if (settings.mode == ARM_MODE_STAY()) {
+        operation = OPERATION_ARM_STAY()
+    } else if (settings.mode == ARM_MODE_NIGHT()) {
+        operation = OPERATION_ARM_NIGHT()
+    } else {
+        log.error "Cannot arm system. Unknown switch mode ${settings.mode}."
+        return
+    }
+
     performOperation(operation)
 }
 
 def off() {
     log.info "Received request to disarm system. Mode: ${settings.mode}"
     def systemStatus = getSystemStatus()
-    if (![STATUS_AWAY_ARMED(), STATUS_STAY_ARMED(), STATUS_EXIT_DELAY()].contains(systemStatus)) {
-    	log.error "Cannot disarm system: System is in invalid state: ${systemStatus}"
+    if (![STATUS_AWAY_ARMED(), STATUS_STAY_ARMED(), STATUS_NIGHT_ARMED(), STATUS_EXIT_DELAY()].contains(systemStatus)) {
+        log.error "Cannot disarm system: system is in invalid state ${systemStatus}"
         return
     }
-    if (systemStatus == STATUS_STAY_ARMED() && settings.mode == ARM_MODE_AWAY()) {
-    	log.error "Cannot disarm system: Expecting status ${STATUS_AWAY_ARMED()}. Actual status: ${systemStatus}"
+    if (systemStatus == STATUS_AWAY_ARMED() && settings.mode != ARM_MODE_AWAY()) {
+        log.error "Cannot disarm system in mode ${systemStatus} via switch in mode ${settings.mode}."
         return
-    } else if (systemStatus == STATUS_AWAY_ARMED() && settings.mode == ARM_MODE_STAY()) {
-    	log.error "Cannot disarm system: Expecting status ${STATUS_STAY_ARMED()}. Actual status: ${systemStatus}"
+    } else if (systemStatus == STATUS_STAY_ARMED() && settings.mode != ARM_MODE_STAY()) {
+        log.error "Cannot disarm system in mode ${systemStatus} via switch in mode ${settings.mode}."
+        return
+    } else if (systemStatus == STATUS_NIGHT_ARMED() && settings.mode != ARM_MODE_NIGHT()) {
+        log.error "Cannot disarm system in mode ${systemStatus} via switch in mode ${settings.mode}."
         return
     }
     performOperation(OPERATION_DISARM())
 }
 
 def refresh() {
-	log.debug "refresh()..."
-	getSystemStatus()
+    log.debug "refresh()..."
+    getSystemStatus()
 }
 
 def ping() {
-	log.debug "ping()..."
-	refresh()
+    log.debug "ping()..."
+    refresh()
 }
 
 def poll() {
-	log.debug "poll()..."
-	refresh()
+    log.debug "poll()..."
+    refresh()
 }
